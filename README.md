4d3e7fe90cb6c9a7d23d25f8786929e549c7f6ca
Doru Moraru <doru.moraru@gmail.com>
2020-06-17 16:26:46 +0100

    added timezone option

--- README.md
+++ README.md
@@ -10,20 +10,25 @@ Server is runing on port 3000
 to:
 
 ```bash
-2019-02-17 20:44:52 ==> Server is runing on port 3000
+2020-05-17 20:44:52.3420 ==> Server is runing on port 3000
 ```
-
 
 ## Installation
 
 ```javascript
-npm i -S console-log-timestamp
+npm i console-log-timestamp
 ```
 
 ## Usage
 
 ```javascript
-require('console-log-timestamp')
+require('console-log-timestamp')();
+```
+
+If you need a specific timezone, just pass it as an argument:
+
+```javascript
+require('console-log-timestamp')('Europe/Paris');
 ```
 
 ---
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-module.exports = (function() {
-    
+const moment = require('moment-timezone');
+module.exports = function(timezone=null) {
+
     // Getting original console.log
     var originalLog = console.log;
 
     // Fn that returns a nicely formated current timestamp
     function getCurrentDate() {
-        return (new Date()).toISOString().replace(/[T|Z]/g, ' ').split('.')[0] + ' ==>';
+        if (timezone) {
+            return moment.tz(timezone).format('YYYY-MM-DD HH:mm:ss.SSSS')
+        }
+        return moment.tz('Europe/London').format('YYYY-MM-DD HH:mm:ss.SSSS') + ' ==>';
     };
-    
+
     // Manipulating originalLog via apply
     console.log = function () {
         var args = [].slice.call(arguments);
         originalLog.apply(console.log, [getCurrentDate()].concat(args));
     };
 
-})();
+};
